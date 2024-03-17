d836527047066d14a3c663184c89534347ae614a
kirrog <kirrog@zertan.ru>
2024-03-17 20:34:34 +0300

    [feature] best result 9.35

--- src/main/java/lab2/MyCrossover.java
+++ src/main/java/lab2/MyCrossover.java
@@ -8,6 +8,8 @@ import java.util.Random;
 
 public class MyCrossover extends AbstractCrossover<double[]> {
     private static double factor = 6.5;
+    private static double prob = 0.95;
+    private static double prob_choose = 0.5;
 
     protected MyCrossover() {
         super(1);
@@ -19,11 +21,23 @@ public class MyCrossover extends AbstractCrossover<double[]> {
         double[] c2 = new double[p2.length];
 
         for (int j = 0; j < p1.length; j++) {
-            if (random.nextDouble() > 0.0) {
+            if (random.nextDouble() > prob) {
                 c1[j] = (p1[j] + p2[j] + (((random.nextDouble() * 10.0) - 5.0) / (factor))) / (2 + (1 / factor));
+            } else {
+                if (random.nextDouble() > prob_choose) {
+                    c1[j] = p1[j];
+                } else {
+                    c1[j] = p2[j];
+                }
             }
-            if (random.nextDouble() > 0.0) {
+            if (random.nextDouble() > prob) {
                 c2[j] = (p1[j] + p2[j] + (((random.nextDouble() * 10.0) - 5.0) / (factor))) / (2 + (1 / factor));
+            } else {
+                if (random.nextDouble() > prob_choose) {
+                    c1[j] = p1[j];
+                } else {
+                    c1[j] = p2[j];
+                }
             }
         }
         children.add(c1);
